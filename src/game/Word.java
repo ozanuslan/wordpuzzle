57a3c33607f7db8fe73f70743d608ccd67ea0686
ozanuslan <ozanuslan@hotmail.com>
2020-04-21 23:18:27 +0300

    Word class

---
+++ src/game/Word.java
@@ -0,0 +1,26 @@
+package game;
+
+public class Word {
+    String word, meaning;
+
+    public Word(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
