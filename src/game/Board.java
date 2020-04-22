3984be24106ff96aa6d1a426ca2cadea79f23c3a
ozanuslan <ozanuslan@hotmail.com>
2020-04-22 21:38:25 +0300

    Need constructor polymorphism

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,5 +1,33 @@
 package game;
 
-public class Board {
+import file.Read;
 
+public class Board {
+    public static final int ROWCOUNT = 15;
+    public static final int ROWLENGTH = 15;
+    private String[][] board;
+
+    public Board(String puzzlePath){
+        board = Read.readBoard(puzzlePath);
+    }
+
+    public Board(String solutionPath){ //Ask for polimorphism
+        board = Read.readSolution(solutionPath);
+    }
+
+    public String[][] getBoard(){
+        return board;
+    }
+
+    public void displayBoard(int x, int y){
+        int rowCount = board.length;
+        int colCount;
+        for(int i =0; i < rowCount; i++){
+            colCount = board[i].length;
+            for(int j = 0; j < colCount; j++){
+                Console.setCursorPosition(x+j, y+i);
+                Console.print(board[i][j]);
+            }
+        }
+    }
 }
