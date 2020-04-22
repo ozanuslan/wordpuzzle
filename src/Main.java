84f4b8c4089e02541f00b06cc801e26eaba03e53
ozanuslan <ozanuslan@hotmail.com>
2020-04-22 23:26:17 +0300

    new constructor fix

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import game.Game;
 
 public class Main {
     public static void main(String[] args) {
-        Game g = new Game("puzzle.txt");
+        Game g = new Game("puzzle.txt", "solution.txt", "word_list.txt", "high_score_table.txt");
         g.run();
     }
 }
