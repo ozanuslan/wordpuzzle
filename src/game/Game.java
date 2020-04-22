f0f58feef98c86fcfec6da33097ecee245f45f24
ozanuslan <ozanuslan@hotmail.com>
2020-04-22 23:01:27 +0300

    Board contents are not changed for sake of display

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,13 +2,15 @@ package game;
 
 import linkedlist.*;
 
-public class Game{
+public class Game {
     Board b;
-    public Game(String puzzlePath){
+
+    public Game(String puzzlePath) {
         Console.getConsole("H", 60, 30, 15, 1);
         b = new Board(puzzlePath);
     }
-    public void run(){
-        b.displayBoard(0, 0);
+
+    public void run() {
+        b.displayBoard(0, 0, true);
     }
 }
