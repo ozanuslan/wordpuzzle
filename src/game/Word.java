f135d97f48e71a8631fda2b952dbbfa8f1325a36
ozanuslan <ozanuslan@hotmail.com>
2020-04-23 00:02:03 +0300

    completion state var added

--- src/game/Word.java
+++ src/game/Word.java
@@ -2,10 +2,12 @@ package game;
 
 public class Word {
     String word, meaning;
-
+    boolean isComplete;
+    
     public Word(String word, String meaning) {
         this.word = word;
         this.meaning = meaning;
+        isComplete = false;
     }
 
     public String getWord() {
@@ -22,5 +24,13 @@ public class Word {
 
     public void setMeaning(String meaning) {
         this.meaning = meaning;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean isComplete) {
+        this.isComplete = isComplete;
     }
 }
