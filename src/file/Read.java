0e22ed904f4b0e8f41f6292d720f1f6e2e52391e
ozanuslan <ozanuslan@hotmail.com>
2020-04-23 19:12:28 +0300

    Highscore can be read now

--- src/file/Read.java
+++ src/file/Read.java
@@ -12,6 +12,7 @@ import game.Console;
 
 public class Read {
     private static final int DICTIONARYLIMIT = 100;
+    private static final int HIGHSCOREPLAYERLIMIT = 10;
 
     public static String[][] readBoard(String boardPath) {
         String[][] board = new String[Board.ROWCOUNT][Board.ROWLENGTH];
@@ -51,7 +52,7 @@ public class Read {
         return null;
     }
 
-    //TODO: Create a trace algorithm for words on the board
+    //TODO: Create a trace algorithm for finding words on the board
     public static SLL readSolutionWordList(String solutionPath) {
         Board solution = new Board(solutionPath);
         SLL solutionWordList = new SLL();
@@ -63,8 +64,8 @@ public class Read {
         try {
             Scanner sc = new Scanner(new File(wordPath));
             int lineCount = 0;
-            String[] word;
-            SLL errorLines = new SLL();
+            String[] wordData;
+            SLL errorList = new SLL();
             while (sc.hasNextLine()) {
                 lineCount++;
                 if (lineCount > DICTIONARYLIMIT) {
@@ -73,22 +74,22 @@ public class Read {
                             + "th word will not be included in the game.", Console.redonblack);
                     break;
                 }
-                word = sc.nextLine().split(",");
-                if (word.length != 2) {
-                    errorLines.insert(lineCount);
+                wordData = sc.nextLine().split(",");
+                if (wordData.length != 2) {
+                    errorList.insert(lineCount);
                 } else {
-                    if (word[0].length() < 2 || word[1].length() < 2) {
-                        errorLines.insert(lineCount);
+                    if (wordData[0].length() < 2 || wordData[1].length() < 2) {
+                        errorList.insert(lineCount);
                     } else {
-                        //TODO: insert words with alphabetical order (function must be in MLL class)
-                        wordList.addWordAlphabetically(new Word(word[0].toLowerCase(), word[1]));
+                        wordList.addWordAlphabetically(new Word(wordData[0].toLowerCase(), wordData[1]));
                     }
                 }
             }
-            if(errorLines.size() > 0){
+            if (errorList.size() > 0) {
                 Console.setCursorPosition(0, 0);
-                Console.print("Incorrect word(s) in "+wordPath.substring(0, wordPath.indexOf("."))+" on line(s): ", Console.redonblack);
-                errorLines.display();
+                Console.print("Incorrect word(s) in " + wordPath.substring(0, wordPath.indexOf(".")) + " on line(s): ",
+                        Console.redonblack);
+                errorList.display();
             }
         } catch (FileNotFoundException e) {
             Scanner sc = new Scanner(System.in);
@@ -102,6 +103,48 @@ public class Read {
 
     public static DLL readHighScoreTable(String highscorePath) {
         DLL highScoreTable = new DLL();
+
+        try {
+            Scanner sc = new Scanner(new File(highscorePath));
+            String[] playerData;
+            int playerCount = 0;
+            SLL errorList = new SLL();
+            while (sc.hasNextLine()) {
+                playerCount++;
+                if (playerCount > HIGHSCOREPLAYERLIMIT) {
+                    Console.setCursorPosition(0, 30);
+                    Console.print(highscorePath.substring(0, highscorePath.indexOf(".")) + " has more than "
+                            + HIGHSCOREPLAYERLIMIT + " players. Players exceeding the limit will not be considered.",
+                            Console.redonblack);
+                } else {
+                    playerData = sc.nextLine().split(";");
+                    if (playerData.length != 2) {
+                        errorList.insert(playerCount);
+                    } else {
+                        if (tryParseInt(playerData[1])) {
+                            highScoreTable.add(new User(playerData[0], Integer.parseInt(playerData[1])));
+                        } else {
+                            errorList.insert(playerCount);
+                        }
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            Scanner sc = new Scanner(System.in);
+            Console.println(e.toString(), Console.redonblack);
+            sc.nextLine();
+            sc.close();
+            System.exit(1);
+        }
         return highScoreTable;
+    }
+
+    private static boolean tryParseInt(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
