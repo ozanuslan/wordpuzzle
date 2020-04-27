c8fe5d4342fe03f160317677730719706a9f1b96
ozanuslan <ozanuslan@hotmail.com>
2020-04-27 17:08:21 +0300

    Word list moved to Game class

--- src/game/Game.java
+++ src/game/Game.java
@@ -19,9 +19,9 @@ public class Game {
     private MLL checkList; // MLL to check words before they are completed
     private DLL highScoreTable;
 
-    public static final int WINX = 80;
+    public static final int WINX = 120;
     public static final int WINY = 30;
-    private final int FONTSIZE = 22;
+    private final int FONTSIZE = 18;
     private final int FONTNO = 2;
     private Coordinate playerPos;
 
@@ -34,9 +34,9 @@ public class Game {
         puzzle = new Board(puzzlePath);
         solution = new Board(solutionPath);
         wordList = Input.readWordList(wordPath);
-        Input.readSolutionWordList(solution, puzzle, wordList); //Sets words in the wordlist as solution if they exist in solution board
-        checkList = Input.readCheckList(wordList);
         wordCoords = Input.getSolutionWordCoords(puzzle);
+        Input.markSolutionWords(solution, wordCoords, wordList); //Sets words in the wordlist as solution if they exist in solution board
+        checkList = Input.readCheckList(wordList);
         highScoreTable = Input.readHighScoreTable(highscorePath);
         playerPos = new Coordinate(7, 7);
     }
@@ -89,9 +89,80 @@ public class Game {
         }
     }
 
+    public void printSolutionWords(int x, int y, boolean hasDisplayFrame) {
+        int generalOffset;
+        if (hasDisplayFrame) {
+            generalOffset = 1;
+            printWordFrame(x, y);
+        } else {
+            generalOffset = 0;
+        }
+        int listSize = wordList.size();
+        int maxWordLen = -1;
+        int rowOffset = 0;
+        int columnOffset = 0;
+        for (int i = 0; i < listSize; i++) {
+            //if (((Word) wordList.getElementByIndex(i)).isSolution()) {
+            Console.setCursorPosition(x + generalOffset + columnOffset, y + generalOffset + rowOffset);
+            if (((Word) wordList.get(i)).isComplete()) {
+                Console.print("[X]");
+            } else {
+                Console.print("[ ]");
+            }
+            Console.print(((Word) wordList.get(i)).getWord().toUpperCase());
+            rowOffset++;
+            maxWordLen = Math.max(maxWordLen, ((Word) wordList.get(i)).getWord().length());
+            if (rowOffset >= Board.ROWCOUNT  && i != listSize - 1) {
+                rowOffset = 0;
+                columnOffset += maxWordLen+3;
+                maxWordLen = 0;
+            }
+            //}
+        }
+    }
+
+    private void printWordFrame(int x, int y) {
+        int frameRowCount = Board.ROWCOUNT + 2;
+        int frameRowLength = Board.ROWLENGTH * 7;
+        for (int i = 0; i < frameRowCount; i++) {
+            for (int j = 0; j < frameRowLength; j++) {
+                Console.setCursorPosition(x + j, y + i);
+                if (i == 0 && j == 0) {
+                    Console.print("╔");
+                } else if (i == 0 && j == 1) {
+                    Console.print("WORD-LIST");
+                    j += 8;
+                } else if (i == 0 && j == frameRowLength - 1) {
+                    Console.print("╗");
+                } else if (i == frameRowCount - 1 && j == 0) {
+                    Console.print("╚");
+                } else if (i == frameRowCount - 1 && j == frameRowLength - 1) {
+                    Console.print("╝");
+                } else if ((i == 0 && j > 8 && j < frameRowLength - 1)
+                        || (i == frameRowCount - 1 && j > 0 && j < frameRowLength - 1)) {
+                    Console.print("═");
+                } else if ((j == 0 || j == frameRowLength - 1) && (i > 0 && i < frameRowCount - 1)) {
+                    Console.print("║");
+                }
+            }
+        }
+    }
+
+    public void displayUnusedWords(int x, int y) {
+        int wordSize = wordList.size();
+        Console.setCursorPosition(x, y);
+        Console.print("Unused words: ");
+        for (int i = 0; i < wordSize; i++) {
+            if (!((Word) wordList.get(i)).isComplete()) {
+                Console.print(((Word) wordList.get(i)).getWord() + " ");
+            }
+        }
+    }
+
     private void printCursor(int px, int py) {
         Console.setCursorPosition(playerPos.getX() + 1, playerPos.getY() + 1);
-        if (puzzle.getBoard()[playerPos.getY()][playerPos.getX()].equals("0") || puzzle.getBoard()[playerPos.getY()][playerPos.getX()].equals("1")) {
+        if (puzzle.getBoard()[playerPos.getY()][playerPos.getX()].equals("0")
+                || puzzle.getBoard()[playerPos.getY()][playerPos.getX()].equals("1")) {
             Console.print("█", Console.greenonblack);
         } else {
             Console.print(puzzle.getBoard()[playerPos.getY()][playerPos.getX()].toUpperCase(), Console.blackongreen);
@@ -100,16 +171,16 @@ public class Game {
 
     public void run() throws InterruptedException {
         // menu();
-        puzzle.displayBoard(0, 0, true);
+        puzzle.printBoard(0, 0, true);
         // solution.displayBoard(17, 0, true);
-        wordList.displaySolutionWords(17, 0, true);
+        printSolutionWords(17, 0, true);
         while (true) {
-            puzzle.displayBoard(1, 1, false);
+            puzzle.printBoard(1, 1, false);
             printCursor(playerPos.getX(), playerPos.getY());
             takeKeyPress();
             Thread.sleep(20);
         }
-        // wordList.displayUnusedWords(0, 0);
+        //displayUnusedWords(0, 0);
         // checkList.display(Console.greenonblack);
         // highScoreTable.displayFromHead(0,0);
     }
