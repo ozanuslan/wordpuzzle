c98c540d8e9371441fc7495f3a4496cc02bc01a4
ozanuslan <ozanuslan@hotmail.com>
2020-05-10 16:15:48 +0300

    Moved variables are changed

--- src/file/Output.java
+++ src/file/Output.java
@@ -6,14 +6,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import game.User;
+import game.Game;
 
 public class Output {
     public static void writeHighScoreTable(DLL highScoreTable, String path) throws IOException {
         PrintWriter pw = new PrintWriter(new FileWriter(new File(path)));
-        for (int i = 0; i < Input.HIGHSCOREPLAYERLIMIT; i++) {
+        for (int i = 0; i < Game.HIGHSCOREPLAYERLIMIT; i++) {
             String name = ((User) highScoreTable.get(i)).getName();
             int score = ((User) highScoreTable.get(i)).getScore();
-            if (i == Input.HIGHSCOREPLAYERLIMIT - 1) {
+            if (i == Game.HIGHSCOREPLAYERLIMIT - 1) {
                 pw.print(name + ";" + score);
             } else {
                 pw.print(name + ";" + score + "\n");
